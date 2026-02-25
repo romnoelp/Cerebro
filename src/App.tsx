3074783b036ffc99dcf75a711f5630cfc0ba9469
Romnoelp Petracorta <petracorta.rmnl@gmail.com>
2026-02-25 10:38:23 +0800

    Build: Project initialization.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { HexagonBackground } from "./components/animate-ui/components/backgrounds/hexagon";
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen">
+      <HexagonBackground />
+    </div>
+  );
+};
+
+export default App;
